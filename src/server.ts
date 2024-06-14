e7a8c248eb3eec67446816cc361f96fc52508460
GuilhermeMarques1 <g-marques@outlook.com>
2024-06-13 21:35:40 -0300

    refactor: Separate app from server

--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,5 @@
-import fastify from 'fastify'
 import { env } from './env'
-import { userRoutes } from './routes/users'
-
-const app = fastify()
-
-app.register(userRoutes, {
-  prefix: 'users',
-})
+import { app } from './app'
 
 app
   .listen({
