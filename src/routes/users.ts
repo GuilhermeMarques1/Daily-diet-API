864f1946600e727e5f29bf94d0ec42bb42c25a06
GuilhermeMarques1 <g-marques@outlook.com>
2024-06-20 16:55:44 -0300

    feat: Add login route

--- src/routes/users.ts
+++ src/routes/users.ts
@@ -9,9 +9,10 @@ export async function userRoutes(app: FastifyInstance) {
       const userBodySchema = z.object({
         name: z.string(),
         email: z.string(),
+        password: z.string(),
       })
 
-      const { name, email } = userBodySchema.parse(req.body)
+      const { name, email, password } = userBodySchema.parse(req.body)
 
       const emailExists = await knex('users').where('email', email).first()
 
@@ -25,12 +26,43 @@ export async function userRoutes(app: FastifyInstance) {
         id: randomUUID(),
         name,
         email,
+        password,
       })
 
       return reply.status(201).send()
     } catch (error) {
       console.error('Error to create user: ', error)
       return reply.status(500).send('Error to create user')
+    }
+  })
+
+  app.post('/login', async (req, reply) => {
+    try {
+      const userBodySchema = z.object({
+        email: z.string(),
+        password: z.string(),
+      })
+
+      const { email, password } = userBodySchema.parse(req.body)
+
+      const user = await knex('users')
+        .where('email', email)
+        .andWhere('password', password)
+        .first()
+
+      if (!user) {
+        return reply
+          .status(400)
+          .send('User does not exist or credentials are incorrect')
+      }
+
+      const userId = user.id
+      reply.cookie('userId', userId)
+
+      return reply.status(200).send()
+    } catch (error) {
+      console.error('Error to login: ', error)
+      return reply.status(500).send('Error to login')
     }
   })
 }
