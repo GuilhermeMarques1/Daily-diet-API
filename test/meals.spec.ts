cda2aac53efbb2d7a2d3b01358e37436dd242770
GuilhermeMarques1 <g-marques@outlook.com>
2024-06-22 19:51:03 -0300

    dev: Test get all meals

--- test/meals.spec.ts
+++ test/meals.spec.ts
@@ -1,8 +1,7 @@
 import { execSync } from 'node:child_process'
-import { afterAll, beforeAll, describe, it } from 'vitest'
-import { app } from '../src/app'
-import { beforeEach } from 'node:test'
+import { it, describe, beforeAll, afterAll, beforeEach, expect } from 'vitest'
 import supertest from 'supertest'
+import { app } from '../src/app'
 
 describe('Meals routes', () => {
   beforeAll(async () => {
@@ -49,5 +48,72 @@ describe('Meals routes', () => {
         diet: true,
       })
       .expect(201)
+  })
+
+  it('should get all meals', async () => {
+    await supertest(app.server)
+      .post('/users')
+      .send({
+        name: 'User test',
+        email: 'test@dev.com',
+        password: 'pass123',
+      })
+      .expect(201)
+
+    const loginUserResponse = await supertest(app.server)
+      .post('/users/login')
+      .send({
+        email: 'test@dev.com',
+        password: 'pass123',
+      })
+      .expect(200)
+
+    const cookies = loginUserResponse.get('Set-Cookie') || []
+
+    await supertest(app.server)
+      .post('/meals')
+      .set('Cookie', cookies)
+      .send({
+        name: 'meal 1',
+        description: 'lorem ipsum',
+        date: '2024-06-21',
+        time: '15:40:00',
+        diet: true,
+      })
+      .expect(201)
+
+    await supertest(app.server)
+      .post('/meals')
+      .set('Cookie', cookies)
+      .send({
+        name: 'meal 2',
+        description: 'lorem ipsum',
+        date: '2024-06-22',
+        time: '19:46:00',
+        diet: false,
+      })
+      .expect(201)
+
+    const getAllMealsResponse = await supertest(app.server)
+      .get('/meals')
+      .set('Cookie', cookies)
+      .expect(200)
+
+    expect(getAllMealsResponse.body).toEqual([
+      expect.objectContaining({
+        name: 'meal 1',
+        description: 'lorem ipsum',
+        date: '2024-06-21',
+        time: '15:40:00',
+        diet: 1,
+      }),
+      expect.objectContaining({
+        name: 'meal 2',
+        description: 'lorem ipsum',
+        date: '2024-06-22',
+        time: '19:46:00',
+        diet: 0,
+      }),
+    ])
   })
 })
