6283f38fb9be8245a71f272a4576e7de6182ca2f
GuilhermeMarques1 <g-marques@outlook.com>
2024-06-22 20:36:36 -0300

    dev: Test update a meal

--- test/meals.spec.ts
+++ test/meals.spec.ts
@@ -165,6 +165,66 @@ describe('Meals routes', () => {
     })
   })
 
+  it('should update a meal', async () => {
+    await supertest(app.server)
+      .post('/users')
+      .send({
+        name: 'User test',
+        email: 'test@dev.com',
+        password: 'pass123',
+      })
+      .expect(201)
+
+    const loginUserResponse = await supertest(app.server)
+      .post('/users/login')
+      .send({
+        email: 'test@dev.com',
+        password: 'pass123',
+      })
+      .expect(200)
+
+    const cookies = loginUserResponse.get('Set-Cookie') || []
+
+    const createMealResponse = await supertest(app.server)
+      .post('/meals')
+      .set('Cookie', cookies)
+      .send({
+        name: 'meal 1',
+        description: 'lorem ipsum',
+        date: '2024-06-21',
+        time: '15:40:00',
+        diet: true,
+      })
+      .expect(201)
+
+    const { id } = createMealResponse.body
+
+    await supertest(app.server)
+      .put(`/meals/${id}`)
+      .set('Cookie', cookies)
+      .send({
+        name: 'meal updated',
+        description: 'lorem ipsum dolor sit amet',
+        date: '2024-06-22',
+        time: '20:30:00',
+        diet: false,
+      })
+      .expect(200)
+
+    const getMealResponse = await supertest(app.server)
+      .get(`/meals/${id}`)
+      .set('Cookie', cookies)
+
+    expect(getMealResponse.body).toEqual({
+      id,
+      name: 'meal updated',
+      description: 'lorem ipsum dolor sit amet',
+      date: '2024-06-22',
+      time: '20:30:00',
+      diet: 0,
+    })
+  })
+
   it('should delete a meal', async () => {
     await supertest(app.server)
       .post('/users')
