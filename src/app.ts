aa7c525bb6e31393e9a04325a54ab43abaf50b75
GuilhermeMarques1 <g-marques@outlook.com>
2024-06-23 00:12:52 -0300

    feat: Add route to return metrics

--- src/app.ts
+++ src/app.ts
@@ -2,6 +2,7 @@ import fastify from 'fastify'
 import { userRoutes } from './routes/users'
 import cookie from '@fastify/cookie'
 import { mealRoutes } from './routes/meals'
+import { metricsRoutes } from './routes/metrics'
 
 export const app = fastify()
 
@@ -12,4 +13,7 @@ app.register(userRoutes, {
 })
 app.register(mealRoutes, {
   prefix: 'meals',
+})
+app.register(metricsRoutes, {
+  prefix: 'metrics',
 })
---
+++ src/routes/metrics.ts
@@ -0,0 +1,39 @@
+import { FastifyInstance } from 'fastify'
+import { checkUserIdIsValid } from '../middleware/check-user-id-is-valid'
+import { knex } from '../database'
+import { Meals } from '../types/Meals'
+
+export async function metricsRoutes(app: FastifyInstance) {
+  app.addHook('preHandler', checkUserIdIsValid)
+
+  app.get('/', async (req, reply) => {
+    const { userId } = req.cookies
+
+    const meals: Meals[] = await knex('meals').where('user_id', userId)
+
+    const metrics = meals.reduce(
+      (acc, meal) => {
+        if (meal.diet) {
+          acc.diet += 1
+        } else {
+          acc.outDiet += 1
+        }
+
+        return acc
+      },
+      {
+        diet: 0,
+        outDiet: 0,
+      },
+    )
+
+    const percentageValue = ((metrics.diet / meals.length) * 100).toFixed(2)
+
+    return reply.status(200).send({
+      total: meals.length,
+      diet: metrics.diet,
+      outDiet: metrics.outDiet,
+      percentageOnDiet: percentageValue + '%',
+    })
+  })
+}
